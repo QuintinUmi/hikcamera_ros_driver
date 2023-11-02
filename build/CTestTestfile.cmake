8b932167cf5a29c065981b42a3ca73b97b860f8b
QIN Qijun <90885055+QuintinUmi@users.noreply.github.com>
2023-11-02 21:12:09 +0800

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/quintinumi/project/ws_hikcamera_ros_driver/src
-# Build directory: /home/quintinumi/project/ws_hikcamera_ros_driver/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("msg_hikcamera")
--- build/msg_hikcamera/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/quintinumi/project/ws_hikcamera_ros_driver/src/msg_hikcamera
-# Build directory: /home/quintinumi/project/ws_hikcamera_ros_driver/build/msg_hikcamera
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
