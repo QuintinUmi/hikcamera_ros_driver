948174bbffd7d109a534571a89fec519110b66ba
QIN Qijun <90885055+QuintinUmi@users.noreply.github.com>
2023-11-02 20:56:23 +0800

    Create msg_hikcamera_grabbing.h

---
+++ src/msg_hikcamera/include/msg_hikcamera_grabbing.h
@@ -0,0 +1,18 @@
+#ifndef _MSG_HIKCAMERA_GRABBING_H_
+#define _MSG_HIKCAMERA_GRABBING_H_
+
+#include "MvCameraControl.h"
+
+typedef struct _CAMERA_INIT_INFO_
+{
+    void* pUser;
+    unsigned char* pData;
+    unsigned int nDataSize;
+    MV_FRAME_OUT stImageInfo;
+
+    unsigned char* pIamgeCache;
+
+}CAMERA_INIT_INFO;
+
+
+#endif 
