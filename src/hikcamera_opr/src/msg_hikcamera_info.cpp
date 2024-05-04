e346f5b163893f2bf8a893b768940d4b2085fddc
QuintinUmi <qqj030212@gmail.com>
2024-05-04 22:43:53 +0800

    Build CameraInfo Publication

--- src/hikcamera_opr/src/msg_hikcamera_info.cpp
+++ src/hikcamera_opr/src/msg_hikcamera_info.cpp
@@ -24,6 +24,7 @@ sensor_msgs::CameraInfo getCameraInfo(ros::NodeHandle rosHandle)
         int width;
         int height;
         cv::Mat cameraMatrix;
+        cv::Mat newCameraMatrix;
         cv::Mat disCoffes;
 
     }cameraInfoMat;
@@ -34,40 +35,43 @@ sensor_msgs::CameraInfo getCameraInfo(ros::NodeHandle rosHandle)
     rosHandle.param("camera_instrinsics_path_yaml", cameraIntrinsicsPath, cv::String("~/caliberation_param.yaml"));
     cv::FileStorage fs(cameraIntrinsicsPath, cv::FileStorage::READ);
     fs["imageWidth"] >> cameraInfoMat.width;
-    fs["imageHeight"] >> cameraInfoMat.D;
-    fs["cameraMatrix"] >> cameraInfo.K;
-    fs["disCoffes"] >> cameraInfo.D;
+    fs["imageHeight"] >> cameraInfoMat.height;
+    fs["cameraMatrix"] >> cameraInfoMat.cameraMatrix;
+    fs["newCameraMatrix"] >> cameraInfoMat.newCameraMatrix;
+    fs["disCoffes"] >> cameraInfoMat.disCoffes;
     fs.release();
 
-    cameraInfo.height 
-    rosHandle.param("ros_image_publish_rate", loopRate, 100);
-    vector<double> D{0.000094, -0.011701, 0.000383, -0.000507, 0.000000};
+    std::vector<double> D{cameraInfoMat.disCoffes.at<double>(0), 
+                        cameraInfoMat.disCoffes.at<double>(1), 
+                        cameraInfoMat.disCoffes.at<double>(2), 
+                        cameraInfoMat.disCoffes.at<double>(3), 
+                        cameraInfoMat.disCoffes.at<double>(4)};
     boost::array<double, 9> K = {
-        404.005825, 0.000000, 335.580380,
-        0.000000, 404.368809, 250.727020,
-        0.000000, 0.000000, 1.000000  
+        cameraInfoMat.cameraMatrix.at<double>(0, 0), cameraInfoMat.cameraMatrix.at<double>(0, 1), cameraInfoMat.cameraMatrix.at<double>(0, 2),
+        cameraInfoMat.cameraMatrix.at<double>(1, 0), cameraInfoMat.cameraMatrix.at<double>(1, 1), cameraInfoMat.cameraMatrix.at<double>(1, 2),
+        cameraInfoMat.cameraMatrix.at<double>(2, 0), cameraInfoMat.cameraMatrix.at<double>(2, 1), cameraInfoMat.cameraMatrix.at<double>(2, 2)
     };
-    
-     boost::array<double, 12> P = {
-        402.124725, 0.000000, 335.482488, 0.000000,
-        0.000000, 403.765045, 250.954855, 0.000000,
-        0.000000, 0.000000, 1.000000, 0.000000
+    boost::array<double, 9> R = {1, 0, 0, 0, 1, 0, 0, 0, 1};
+    boost::array<double, 12> P = {
+        cameraInfoMat.newCameraMatrix.at<double>(0, 0), cameraInfoMat.newCameraMatrix.at<double>(0, 1), cameraInfoMat.newCameraMatrix.at<double>(0, 2), 0,
+        cameraInfoMat.newCameraMatrix.at<double>(1, 0), cameraInfoMat.newCameraMatrix.at<double>(1, 1), cameraInfoMat.newCameraMatrix.at<double>(1, 2), 0, 
+        cameraInfoMat.newCameraMatrix.at<double>(2, 0), cameraInfoMat.newCameraMatrix.at<double>(2, 1), cameraInfoMat.newCameraMatrix.at<double>(2, 2), 0
     };
-    boost::array<double, 9> r = {1, 0, 0, 0, 1, 0, 0, 0, 1};
 
-    cam.height = 480;
-    cam.width = 640;
-    cam.distortion_model = "plumb_bob";
-    cam.D = D;
-    cam.K = K;
-    cam.P = P;
-    cam.R = r;
-    cam.binning_x = 0;
-    cam.binning_y = 0;
-    cam.header.frame_id = "camera";  //frame_id为camera，也就是相机名字
-    cam.header.stamp = ros::Time::now();
-    cam.header.stamp.nsec = 0;
-    return cam;
+    cameraInfo.width = cameraInfoMat.width;
+    cameraInfo.height = cameraInfoMat.height;
+    cameraInfo.distortion_model = "plumb_bob";
+    cameraInfo.D = D;
+    cameraInfo.K = K;
+    cameraInfo.P = P;
+    cameraInfo.R = R;
+    cameraInfo.binning_x = 0;
+    cameraInfo.binning_y = 0;
+    cameraInfo.header.frame_id = "";  
+    cameraInfo.header.stamp = ros::Time::now();
+    cameraInfo.header.stamp.nsec = 0;
+
+    return cameraInfo;
 
 }
 
@@ -81,18 +85,13 @@ int main(int argc, char *argv[])
     sensor_msgs::CameraInfo cameraInfo;
     ros::Publisher infoPub = rosHandle.advertise<sensor_msgs::CameraInfo>("/msg_camera/info", 1000);
 
-    // ros::Publisher imgPub = rosHandle.advertise<sensor_msgs::Image>("/msg_camera/img", 100);
-    // ros::Publisher msgPub = rosHandle.advertise<std_msgs::String>("/msg_camera/std_msgs", 100);
-
-    int loopRate = 1;
+    int loopRate = 10;
     ros::Rate loop_rate(loopRate);
-    
-    
 
     while(ros::ok()){
 
-
-        imgPub.publish(imgMsg);
+        cameraInfo = getCameraInfo(rosHandle);
+        infoPub.publish(cameraInfo);
         ros::spinOnce();
         loop_rate.sleep();
     }
